4f7071dae12d9b9769250e046c4c91bb6d920cf9
Muthukumar Chidambaram <ezhaivendan@gmail.com>
2022-08-14 21:03:34 +0530

    Fetch Users from API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Scripts
+class loadUser {
+    constructor(users) {
+        this.users = users;
+    }
+    // set category into html select box.
+    loadUserIntoHtml() {
+        console.log('users', this.users)
+    }
+}
+
+async function fetchUserFromAPI() {
+    let jsonRes = await fetch('https://randomuser.me/api/?results=20');
+    let response = await jsonRes.json();
+    return formuserResponse(response);
+}
+
+function formuserResponse(users) {
+    new loadUser(users.results).loadUserIntoHtml();
+}
+
+fetchUserFromAPI()
