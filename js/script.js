92b9fccd13cd82f75964da04f8082eec103cffbb
Muthukumar Chidambaram <ezhaivendan@gmail.com>
2022-09-05 23:16:52 +0530

    Removed unwanted code

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
 // Scripts
-// Get User starts here
-
-// class loadUser {
-//     constructor(users) {
-//         this.users = users;
-//     }
-//     // set category into html select box.
-//     loadUserIntoHtml() {
-//         console.log('this.users', this.users)
-//         let userRows = [];
-//         let userRowForm = this.users.map(user => `<div class="user-box">
-//             <img src=${user.picture.medium} />
-//             <div class="user_name">${user.name.title} ${user.name.first} ${user.name.last}</div>
-//             <div class="user_detail">${user.dob.age}, ${user.location.country}</div>
-//         </div>`)
-    
-//         document.getElementById('user_container').innerHTML = userRowForm.join('');
-//     }
-// }
-
-// let userArray = [];
-
 
 async function fetchUserFromAPI() {
     let jsonRes = await fetch('https://randomuser.me/api/?results=20');
@@ -35,15 +13,6 @@ async function fetchUserFromAPI() {
         </div>`)
     
     document.getElementById('user_container').innerHTML = userRowForm.join('');
-    // let userArr = JSON.parse(localStorage.getItem("user"));
-    // console.log('user from local', userArr)
-    //return formuserResponse(response);
 }
 
 fetchUserFromAPI()
-
-// function formuserResponse(users) {
-//     new loadUser(users.results).loadUserIntoHtml();
-// }
-
-// fetchUserFromAPI()
