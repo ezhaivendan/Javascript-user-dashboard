369c83d65bc66187b320731a87a99b85ec73663e
Muthukumar Chidambaram <ezhaivendan@gmail.com>
2022-09-10 23:38:39 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let retriveLocalUser;
+document.getElementById("user_details").classList.add("is-hidden");
+document.getElementById("user_details_overlay").classList.add("is-hidden");
 async function fetchUserFromAPI() {
     if(!retriveLocalUser){
     let jsonRes = await fetch('https://randomuser.me/api/?results=20');
@@ -6,9 +8,10 @@ async function fetchUserFromAPI() {
     localStorage.setItem('userArray', JSON.stringify(response));
     retriveLocalUser = JSON.parse(localStorage.getItem('userArray'))
     }
+    console.log('retriveLocalUser', retriveLocalUser);
     let userRowForm = retriveLocalUser.results.map(user => `<div class="user-box">
             <img title="${user.name.title} ${user.name.first} ${user.name.last}" src=${user.picture.large} />
-            <div class="user_name">${user.name.title} ${user.name.first} ${user.name.last}</div>
+            <div class="user_name" onclick='getUserDetails("${user.picture.large}")'>${user.name.title} ${user.name.first} ${user.name.last}</div>
             <div class="user_detail">${user.dob.age}, ${user.location.country}</div>
         </div>`)
     
@@ -16,3 +19,18 @@ async function fetchUserFromAPI() {
 }
 
 fetchUserFromAPI()
+
+function getUserDetails(userImg) {
+    console.log("user", userImg)
+    document.getElementById("user_details").classList.remove("is-hidden");
+    document.getElementById("user_details_overlay").classList.remove("is-hidden");
+    let filterUser = retriveLocalUser.results.filter(user => user.picture.large === userImg);
+    let userDet = filterUser.map(userDet => 
+        `<div class="user-img">
+            <img src=${userDet.picture.large} />
+        </div>`
+    );
+
+    console.log("filterUser", filterUser, "userDet", userDet)
+    document.getElementById('user_details').innerHTML = userDet.join('');
+}
