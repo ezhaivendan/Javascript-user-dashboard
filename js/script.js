58072d0866b7c327f87f35024a32ef3160eeb144
Muthukumar Chidambaram <ezhaivendan@gmail.com>
2022-09-11 15:38:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ async function fetchUserFromAPI() {
     localStorage.setItem('userArray', JSON.stringify(response));
     retriveLocalUser = JSON.parse(localStorage.getItem('userArray'))
     }
+    console.log('retriveLocalUser', retriveLocalUser);
     let userRowForm = retriveLocalUser.results.map(user => `<div class="user-box">
             <img title="${user.name.title} ${user.name.first} ${user.name.last}" src=${user.picture.large} />
             <div class="user_name" onclick='getUserDetails("${user.picture.large}")'>${user.name.title} ${user.name.first} ${user.name.last}</div>
@@ -24,10 +25,19 @@ function getUserDetails(userImg) {
     document.getElementById("user_details_overlay").classList.remove("is-hidden");
     let filterUser = retriveLocalUser.results.filter(user => user.picture.large === userImg);
     let userDet = filterUser.map(userDet => 
-        `<div class="user-img">
-            <img src=${userDet.picture.large} />
+        `<div class="user-popup">
+            <div class="close-icon" onclick="closePopup()"> <span> X </span></div>
+            <div class="user-img">
+                <img src=${userDet.picture.large} />
+            </div>
         </div>`
     );
 
     document.getElementById('user_details').innerHTML = userDet.join('');
+}
+
+function closePopup(){
+    document.getElementById("user_details").classList.add("is-hidden");
+    document.getElementById("user_details_overlay").classList.add("is-hidden");
+    document.getElementById('user_details').innerHTML = "";
 }
